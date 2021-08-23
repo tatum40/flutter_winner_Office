d020c6e15c62b73fe3983a6022aef4f75a4d5419
tatum <w.ntapong@gmail.com>
2021-08-23 17:25:33 +0700

    create page : add new page category & profile

---
+++ lib/categoryMenu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryMenu extends StatelessWidget {
+  const CategoryMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Category'),
+      ),
+    );
+  }
+}
